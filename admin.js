921d71af87967df6a493886190d1f769b55442cb
David Norton <davidnorton@cc0352.cua.local>
2018-02-20 13:30:44 -0700

    admin.js updated to include /sass service for testing, and cua_node_modules removed

--- admin.js
+++ admin.js
@@ -1,15 +1,72 @@
 // First attempt at an Express/Mongo app; sidecar for FreeCodeCamp
 // @author Dave Norton
 
+/*
+  Use the /newuser page to create new users for CodeCamp Workbench
+
+  Use the /sass page to submit JSON-formatted SASS/SCSS and get a response.
+  Expected post format is like this (JSON format):
+
+    {"source":"$test-color: red; test { color: $test-color; }"}
+
+  Expected response:
+
+    {
+      "source": "$test-color: red; test { color: $test-color; }",
+      "result": "test { color: red; } "
+    }
+*/
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8088
 var mongoose = require('mongoose');
 var User = require('./cua/models/user');
 var bodyParser = require('body-parser');
+const sass = require('node-sass');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
+
+app.use((req, res, next) => {
+  res.header({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-type',
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  req.headers['content-type'] = 'application/json';
+  next();
+});
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/sass', (req, res) => res.json(true));
+
+app.post('/sass', (req, res) => {
+  const source = req.body.source;
+  sass.render(
+    {
+      data: source,
+    },
+    (error, result) => {
+      if (error) {
+        return res.json({
+          error,
+          source,
+        });
+      }
+
+      return res.json({
+        source,
+        result: result.css.toString(),
+      });
+    }
+  );
+});
 
 mongoose.connect('mongodb://localhost/freecodecamp', { useMongoClient: true }, function(err) {
   if (err) {
