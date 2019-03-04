34d050fc0c01a157987b1e61a0da179ad631f90a
Tyler Carver <carver230620@cua.edu>
2019-03-04 07:26:43 -0700

    Corrected the name for the boot file for loading controllers.

--- server/boot/webAppControllers.js
+++ server/boot/webAppControllers.js
@@ -3,6 +3,11 @@ var path = require("path");
 var webAppRootPath = path.resolve("server/webApp");
 var directoryWalkerSync = require("../../common/core/fs/directoryWalkerSync");
 
+// NOTE: Controllers should remain small, however, if adding routes in controllers
+//  begins to take too much file space, then a .routes.js model should be used and adding routes
+//  should be moved to those files. Checkout commit: b940d01 Used a loopback router instead ...
+//  for an example.
+
 function boot(app) {
 
 	directoryWalkerSync.walkDirectory(webAppRootPath, null, null, function(filePathName, stats) {
