5b811301df0bc98808cf2c6f8fb7cc28d60ec1a9
Dave Norton <dnorton211153@gmail.com>
2025-04-03 19:05:16 -0600

    variable start script location for auto-startup systemd pm2 (which runs as root)

--- pm2Start.js
+++ pm2Start.js
@@ -4,6 +4,9 @@ var nodemailer = require('nodemailer');
 var moment = require('moment-timezone');
 var _ = require('lodash');
 
+// load environment variables
+var PRODUCTION_START_SCRIPT = process.env.PRODUCTION_START_SCRIPT || 'server/production-start.js'
+
 var instances = process.env.INSTANCES || 1;
 var pm2AppName = process.env.PM2_APP_NAME || 'app';
 var maxMemory = process.env.MAX_MEMORY || '390M';
@@ -11,7 +14,7 @@ var maxMemory = process.env.MAX_MEMORY || '390M';
 pm2.connect(function() {
   pm2.start({
     name: pm2AppName,
-    script: 'server/production-start.js',
+    script: PRODUCTION_START_SCRIPT,
     'exec_mode': 'cluster',
     instances: instances,
     'max_memory_restart': maxMemory,
