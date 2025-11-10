11d5e9c3574e8e0db2e055ffeff32f20f1fa550d
Dave Norton <davidandrewnorton@gmail.com>
2025-11-10 01:34:02 -0700

    fix(branding): utilize hot-reloading in congruence with React namespace and such

--- common/utils/branding.js
+++ common/utils/branding.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const { brandingConfigs, exposedHostname, NS } = require('../config.json');
 var branding = {};
 
-if (brandingConfigs) {
+if (NS && typeof window !== 'undefined' && window[NS] && window[NS].branding) {
+  branding = window[NS].branding;
+} else if (brandingConfigs) {
   var BASE_URL = exposedHostname || 'redrockcode.com'
-  branding = brandingConfigs[BASE_URL] || {}
+  window[NS].branding = branding = brandingConfigs[BASE_URL] || {}
 } else {
   console.warn(
     'No brandingConfigs found in server config.json; using defaults'
