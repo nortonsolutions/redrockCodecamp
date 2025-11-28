8908820456e6d9121a32bcb39beaec248938506f
Dave Norton <davidandrewnorton@gmail.com>
2025-11-28 16:26:21 -0700

    fix(branding): window branding; better failover

--- common/utils/branding.js
+++ common/utils/branding.js
@@ -3,16 +3,26 @@ require('dotenv').config();
 const { brandingConfigs, exposedHostname, NS } = require('../config.json');
 var branding = {};
 
-if (NS && typeof window !== 'undefined' && window[NS] && window[NS].branding) {
-  branding = window[NS].branding;
-} else if (brandingConfigs) {
-  var BASE_URL = exposedHostname || 'redrockcode.com'
-  branding = brandingConfigs[BASE_URL] || {}
-  if (typeof window !== 'undefined') window[NS].branding = branding;
-} else {
-  console.warn(
-    'No brandingConfigs found in server config.json; using defaults'
-  );
+if (NS && typeof window !== 'undefined') {
+
+  if (window[NS] && window[NS].branding && Object.keys(window[NS].branding).length) {
+    branding = window[NS].branding;
+  } else {
+    window[NS] = {};
+    if (brandingConfigs) {
+      var BASE_URL = exposedHostname || 'redrockcode.com'
+      branding = brandingConfigs[BASE_URL] || {}
+      if (Object.keys(branding).length) {
+        window[NS].branding = branding;
+      } else {
+        console.warn(`No branding config found for hostname: ${BASE_URL}; using defaults`);
+        branding = window[NS].branding = {};
+      }
+    } else {
+      console.warn('No brandingConfigs found in server config.json; using defaults');
+      branding = window[NS].branding = {};
+    }
+  }
 }
 
 exports.getBranding = function() {
