8141d3b112b44fd77da3f0bb809d392bf7ffa226
Dave Norton <davidandrewnorton@gmail.com>
2025-11-28 17:28:13 -0700

    feat(prerequisites): Challenges show lock icon and display modal when prerequisites not met

--- common/app/Map/Challenge.jsx
+++ common/app/Map/Challenge.jsx
@@ -11,6 +11,19 @@ import { challengeMapSelector } from '../entities';
 import { Link } from '../Router';
 import { onRouteChallenges } from '../routes/Challenges/redux';
 
+const certificationNames = {
+  'Beginning Html And Css (phase I Term 1)': { name: 'user account', cert: null },
+  'Advanced Html And Css (phase I Term 2)': { name: 'user account', cert: null },
+  'Beginning Javascript (phase I Term 3)': { name: 'Responsive Web Design', cert: 'isRespWebDesignCertified' },
+  'Advanced Javascript (phase Ii Term 4)': { name: 'Responsive Web Design', cert: 'isRespWebDesignCertified' },
+  'Beginning Web Development (phase Ii Term 5)': { name: 'JS Algorithms and Data Structures', cert: 'isJsAlgoDataStructCertified' },
+  'Advanced Web Development (phase Ii Term 6)': { name: 'JS Algorithms and Data Structures', cert: 'isJsAlgoDataStructCertified' },
+  'Front End Libraries (phase Iii Term 7)': { name: 'JavaScript Front-End Web Development', cert: 'isFrontEndCertified' },
+  'Server Side Development (phase Iii Term 8)': { name: 'JavaScript Front-End Libraries', cert: 'isFrontEndLibsCertified' },
+  'Advanced Server Side Development (phase Iii Term 9)': { name: 'JavaScript APIs and Microservices', cert: 'isApisMicroservicesCertified' },
+  'More Javascript For Masochists (phase X)': { name: 'Information Security and QA', cert: 'isInfosecQaCertified' }
+};
+
 const propTypes = {
   block: PropTypes.string,
   challenge: PropTypes.object,
@@ -96,6 +109,22 @@ function makeMapStateToProps(_, { dashedName }) {
 }
 
 export class Challenge extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  handleLockedClick = (e) => {
+    e.preventDefault();
+    this.setState({ showModal: true });
+  }
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  }
+
   renderCompleted(isCompleted, isLocked) {
     if (isLocked || !isCompleted) {
       return null;
@@ -137,6 +166,33 @@ export class Challenge extends PureComponent {
     );
   }
 
+  renderModal(requiredCert) {
+    if (!this.state.showModal) {
+      return null;
+    }
+    return (
+      <div className='modal fade in' style={{ display: 'block' }} role='dialog'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <button type='button' className='close' onClick={this.closeModal}>
+                <span>&times;</span>
+              </button>
+              <h4 className='modal-title'>Certification Required</h4>
+            </div>
+            <div className='modal-body'>
+              <p>You need to obtain the <strong>{requiredCert}</strong> certification before accessing this challenge.</p>
+              <p>Advanced students can email <a href='mailto:dave@silvermedal.net'>dave@silvermedal.net</a> to request an override.</p>
+            </div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-primary' onClick={this.closeModal}>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 
   render() {
     const {
@@ -166,62 +222,96 @@ export class Challenge extends PureComponent {
       return null;
     }
 
-    let hide = false;
+    let certLocked = false;
+    let requiredCertName = null;
     
     switch (superBlock) {
       
         case "Beginning Html And Css (phase I Term 1)":
-          if (!userHasChallengeMap) hide = true;
+          if (!userHasChallengeMap) {
+            certLocked = true;
+            requiredCertName = 'user account';
+          }
           break;
 
         case "Advanced Html And Css (phase I Term 2)":
-          if (!userHasChallengeMap) hide = true;
+          if (!userHasChallengeMap) {
+            certLocked = true;
+            requiredCertName = 'user account';
+          }
           break;  
 
         case "Beginning Javascript (phase I Term 3)":
-          if (!isRespWebDesignCertified) hide = true;
+          if (!isRespWebDesignCertified) {
+            certLocked = true;
+            requiredCertName = 'Responsive Web Design';
+          }
           break;
 
         case "Advanced Javascript (phase Ii Term 4)":
-          if (!isRespWebDesignCertified) hide = true;
+          if (!isRespWebDesignCertified) {
+            certLocked = true;
+            requiredCertName = 'Responsive Web Design';
+          }
           break;
 
         case "Beginning Web Development (phase Ii Term 5)":
-          if (!isJsAlgoDataStructCertified) hide = true;
+          if (!isJsAlgoDataStructCertified) {
+            certLocked = true;
+            requiredCertName = 'JS Algorithms and Data Structures';
+          }
           break;
 
         case "Advanced Web Development (phase Ii Term 6)":
-          if (!isJsAlgoDataStructCertified) hide = true;
+          if (!isJsAlgoDataStructCertified) {
+            certLocked = true;
+            requiredCertName = 'JS Algorithms and Data Structures';
+          }
           break;
 
         case "Front End Libraries (phase Iii Term 7)":
-          if (!isFrontEndCertified) hide = true;
+          if (!isFrontEndCertified) {
+            certLocked = true;
+            requiredCertName = 'JavaScript Front-End Web Development';
+          }
           break;
 
         case "Server Side Development (phase Iii Term 8)":
-          if (!isFrontEndLibsCertified) hide = true;
+          if (!isFrontEndLibsCertified) {
+            certLocked = true;
+            requiredCertName = 'JavaScript Front-End Libraries';
+          }
           break;
 
         case "Advanced Server Side Development (phase Iii Term 9)":
-          if (!isApisMicroservicesCertified) hide = true;
+          if (!isApisMicroservicesCertified) {
+            certLocked = true;
+            requiredCertName = 'JavaScript APIs and Microservices';
+          }
           break;
 
         case "More Javascript For Masochists (phase X)":
-          if (!isInfosecQaCertified) hide = true;
+          if (!isInfosecQaCertified) {
+            certLocked = true;
+            requiredCertName = 'Information Security and QA';
+          }
           break;
     }
     
-    if (! prerequisiteMet) hide = true;
+    if (!prerequisiteMet && !certLocked) {
+      certLocked = true;
+      requiredCertName = 'prerequisite challenge';
+    }
 
     const challengeClassName = classnames({
-      'hidden': hide,
       'text-primary': true,
       'padded-ionic-icon': true,
       'map-challenge-title': true,
-      'ion-checkmark-circled faded': !(isLocked || isComingSoon) && isCompleted,
-      'ion-ios-circle-outline': !(isLocked || isComingSoon) && !isCompleted,
-      'ion-locked': isLocked || isComingSoon,
-      disabled: isLocked || (!isDev && isComingSoon)
+      'ion-checkmark-circled faded': !(isLocked || isComingSoon || certLocked) && isCompleted,
+      'ion-ios-circle-outline': !(isLocked || isComingSoon || certLocked) && !isCompleted,
+      'ion-locked': isLocked || isComingSoon || certLocked,
+      disabled: isLocked || (!isDev && isComingSoon),
+      'slightly-faded': certLocked
     });
     if (isLocked || (!isDev && isComingSoon)) {
       return this.renderLocked(
@@ -231,6 +321,30 @@ export class Challenge extends PureComponent {
         challengeClassName
       );
     }
+    
+    if (certLocked) {
+      return (
+        <div>
+          <div
+            className={ challengeClassName }
+            key={ title }
+            >
+            <a
+              href='#'
+              onClick={ this.handleLockedClick }
+              style={{ cursor: 'pointer' }}
+              >
+              <span>
+                { title }
+                { this.renderRequired(isRequired) }
+              </span>
+            </a>
+          </div>
+          { this.renderModal(requiredCertName) }
+        </div>
+      );
+    }
+    
     return (
       <div
         className={ challengeClassName }
